ce84451ebba5eac0af27078cca76b6486cc64272
iscelor <iscelor@gmail.com>
2025-05-09 19:34:49 +0800

    Update op2.sh

--- diy/mt798x/op2.sh
+++ diy/mt798x/op2.sh
@@ -12,7 +12,5 @@ sed -i "s/ImmortalWrt-2.4G/ASUS/g" package/mtk/applications/mtwifi-cfg/files/mtw
 sed -i "s/ImmortalWrt-5G/ASUS_5G/g" package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
 # 修改 2.4GHz 和 5GHz 的加密方式及密码
-sed -i '/set wireless\.default_[a-zA-Z0-9_]*\.encryption=none/ {
-    s/none/sae-mixed/
-    a\                    set wireless.default_${dev}.key=Dsa78963
-}' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+sed -i '/set wireless.default_${dev}\.encryption=none$/{s/none/sae-mixed/;a\\t\t\t\t\tset wireless.default_${dev}.key=Dsa78963
+}' mtwifi.sh

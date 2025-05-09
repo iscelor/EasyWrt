23da574bf61aa8e16bfd28b6af014a6a9dc96774
iscelor <iscelor@gmail.com>
2025-05-09 20:04:35 +0800

    Update t7op2.sh

--- diy/mt798x/t7op2.sh
+++ diy/mt798x/t7op2.sh
@@ -9,4 +9,4 @@ sed -i 's/192.168.6.1/192.168.5.1/g' package/base-files/files/bin/config_generat
 
 # 修改 2.4GHz 和 5GHz 的加密方式及密码
 sed -i '/set wireless.default_${dev}\.encryption=none$/{s/none/sae-mixed/;a\\t\t\t\t\tset wireless.default_${dev}.key=Dsa78963
-}' mtwifi.sh
+}' package/mtk/applications/mtwifi-cfg/files/mtwifi.sh

6dc52eeee35bc25a7b1d5185d73bca5017059268
iscelor <iscelor@gmail.com>
2025-09-23 10:19:07 +0800

    Update op1.sh

--- diy/mt798x/op1.sh
+++ diy/mt798x/op1.sh
@@ -4,3 +4,5 @@
 #=================================================
 ##添加自己的插件库
 #echo -e "\nsrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk" >> feeds.conf.default
+echo -e "\nsrc-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main" >> feeds.conf.default
+echo -e "\nsrc-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main" >> feeds.conf.default

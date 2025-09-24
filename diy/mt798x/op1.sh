e40bb6556de05fae9bd3053652a6519028e5bf26
iscelor <iscelor@gmail.com>
2025-09-24 14:07:18 +0800

    Update op1.sh

--- diy/mt798x/op1.sh
+++ diy/mt798x/op1.sh
@@ -4,5 +4,8 @@
 #=================================================
 ##添加自己的插件库
 #echo -e "\nsrc-git extraipk https://github.com/xiangfeidexiaohuo/extra-ipk" >> feeds.conf.default
-sed -i '1i src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
-sed -i '1i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
+#sed -i '1i src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main' feeds.conf.default
+#sed -i '1i src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main' feeds.conf.default
+
+##添加OpenClash
+git clone --depth=1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
